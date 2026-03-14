aa73d10b887d2d4711aac51dd4bebabb70709b6f
anjo <anjelobenedict@gmail.com>
2026-03-15 01:11:14 +0800

    deploy css

--- next.config.ts
+++ next.config.ts
@@ -1,3 +1,11 @@
+const repoName = "rizal-filmreview-repo";
+
 export default {
   output: "export",
+  basePath: `/${repoName}`,
+  assetPrefix: `/${repoName}/`,
+  trailingSlash: true,
+  images: {
+    unoptimized: true,
+  },
 };
